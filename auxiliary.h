4faffd08dac204a3bf0bf79647db6445644ecc32
blacksfk <blacksunflowakuttaspam@hotmail.com>
2021-05-21 12:51:26 +1000

    fixed potential bug: wrapped macros in do while (0)

--- auxiliary.h
+++ auxiliary.h
@@ -10,20 +10,20 @@
 /**
  * Delete (free) the provided cJSON object and return NULL.
  */
-#define RET_NULL(x) {\
+#define RET_NULL(x) do {\
 	cJSON_Delete(x);\
 	return NULL;\
-}
+} while(0)
 
 /**
  * Add the number v to the object o under the key k. Deletes o and returns NULL
  * if adding the number failed.
  */
-#define NUM_2_OBJ(o, k, v) {\
+#define NUM_2_OBJ(o, k, v) do {\
 	if (!cJSON_AddNumberToObject(o, k, v)) {\
 		RET_NULL(o);\
 	}\
-}
+} while(0)
 
 /**
  * The same as NUM_2_OBJ but adds the number b to the object o under the key k if p is NULL
@@ -32,23 +32,23 @@
  * Example:
  * NUM_2_OBJ_CMP(obj, "position", prev, prev->position, curr->position)
  */
-#define NUM_2_OBJ_CMP(o, k, p, a, b) {\
+#define NUM_2_OBJ_CMP(o, k, p, a, b) do {\
 	if (!prev || a != b) {\
 		if (!cJSON_AddNumberToObject(o, k, b)) {\
 			RET_NULL(o);\
 		}\
 	}\
-}
+} while(0)
 
 /**
  * Add the boolean v to the object o under the key k. Deletes o and returns NULL
  * if adding the boolean failed.
  */
-#define BOOL_2_OBJ(o, k, v) {\
+#define BOOL_2_OBJ(o, k, v) do {\
 	if (!cJSON_AddBoolToObject(o, k, v)) {\
 		RET_NULL(o);\
 	}\
-}
+} while(0)
 
 /**
  * The same as BOOL_2_OBJ but adds the boolean b to the object o under the key k if p is NULL
@@ -57,13 +57,13 @@
  * Example:
  * BOOL_2_OBJ_CMP(obj, "globalYellow", prev, prev->globalYellow, curr->globalYellow)
  */
-#define BOOL_2_OBJ_CMP(o, k, p, a, b) {\
+#define BOOL_2_OBJ_CMP(o, k, p, a, b) do {\
 	if (!p || a != b) {\
 		if (!cJSON_AddBoolToObject(o, k, b)) {\
 			RET_NULL(o);\
 		}\
 	}\
-}
+} while(0)
 
 cJSON* addWstrToObject(cJSON*, char*, const wchar_t*);
 void* clone(const void*, size_t);
