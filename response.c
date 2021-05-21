3727d43e714ff8150bd8189653b34c7d151dd884
blacksfk <blacksunflowakuttaspam@hotmail.com>
2021-05-21 20:38:25 +1000

    better header handling

--- response.c
+++ response.c
@@ -18,7 +18,8 @@ static struct payload* createPayload() {
 		return NULL;
 	}
 
-	p->len = 0;
+	// length accounts for the null terminator
+	p->len = 1;
 	p->cap = BUF_SIZE;
 	p->data[0] = '\0';
 
@@ -44,9 +45,9 @@ Response* createResponse() {
 		return NULL;
 	}
 
-	r->head = createPayload();
+	r->headers = calloc(HEADER_COUNT, sizeof(Header*));
 
-	if (!r->head) {
+	if (!r->headers) {
 		free(r);
 
 		return NULL;
@@ -56,14 +57,95 @@ Response* createResponse() {
 
 	if (!r->body) {
 		free(r);
-		free(r->head);
+		free(r->headers);
 
 		return NULL;
 	}
 
-	r->code = CURLE_OK;
+	r->status = 0;
+	r->headerCount = 0;
+	r->headerCap = HEADER_COUNT;
+	r->curlCode = CURLE_OK;
 
 	return r;
+}
+
+/**
+ * Allocate memory for a header object.
+ * @param  k Header name (key).
+ * @param  v Header value.
+ */
+static Header* createHeader(const char* k, const char* v) {
+	Header* h = malloc(sizeof(*h));
+
+	if (!h) {
+		return NULL;
+	}
+
+	h->key = strdup(k);
+
+	if (!h->key) {
+		free(h);
+
+		return NULL;
+	}
+
+	h->value = strdup(v);
+
+	if (!h->value) {
+		free(h->key);
+		free(h);
+
+		return NULL;
+	}
+
+	return h;
+}
+
+/**
+ * Allocate memory for a response header object and append it to the array.
+ * @param  r Response object.
+ * @param  k Header name (key).
+ * @param  v Header value.
+ * @return   Allocated and appended header. Returns NULL if memory could not be allocated.
+ */
+Header* addResponseHeader(Response* r, const char* k, const char* v) {
+	Header* h = createHeader(k, v);
+
+	if (!h) {
+		return NULL;
+	}
+
+	if (r->headerCount == r->headerCap) {
+		// reached capacity - re-allocate
+		// current size of the array + size of HEADER_COUNT header objects
+		r->headers = realloc(r->headers, sizeof(r->headers) + sizeof(Header*) * HEADER_COUNT);
+
+		if (!r->headers) {
+			free(h);
+
+			return NULL;
+		}
+
+		r->headerCap += HEADER_COUNT;
+	}
+
+	r->headers[r->headerCount++] = h;
+
+	return h;
+}
+
+/**
+ * Extract various bits of useful information and set it in the response object.
+ * Currently only the status code is extracted.
+ * @param curl
+ * @param r
+ */
+void populateResponse(CURL* curl, Response* r) {
+	long status;
+
+	curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &status);
+	r->status = (int) status;
 }
 
 /**
@@ -71,7 +153,13 @@ Response* createResponse() {
  * @param r
  */
 void freeResponse(Response* r) {
-	freePayload(r->head);
+	for (int i = 0; i < r->headerCount; i++) {
+		free(r->headers[i]->key);
+		free(r->headers[i]->value);
+		free(r->headers[i]);
+	}
+
 	freePayload(r->body);
+	free(r->headers);
 	free(r);
 }
--- response.h
+++ response.h
@@ -1,14 +1,18 @@
 #ifndef RESPONSE_H
 #define RESPONSE_H
 
+#pragma warning(disable:4996)
+
 #include <stdlib.h>
+#include <string.h>
 // classic MS and non-conforming code
 // supress C5105 (undefined behaviour of "defined" in macro expansion) for the windows
 // header files (included by libcurl)
 #pragma warning(disable:5105)
 #include <curl/curl.h>
 
-#define BUF_SIZE 4096
+#define BUF_SIZE 1024
+#define HEADER_COUNT 10
 
 struct payload {
 	char* data;
@@ -16,13 +20,23 @@ struct payload {
 	size_t cap;
 };
 
+typedef struct header {
+	char* key;
+	char* value;
+} Header;
+
 typedef struct response {
-	struct payload* head;
 	struct payload* body;
-	CURLcode code;
+	CURLcode curlCode;
+	int status;
+	int headerCount;
+	int headerCap;
+	Header** headers;
 } Response;
 
 Response* createResponse();
+Header* addResponseHeader(Response*, const char*, const char*);
+void populateResponse(CURL*, Response*);
 void freeResponse(Response* r);
 
 #endif
